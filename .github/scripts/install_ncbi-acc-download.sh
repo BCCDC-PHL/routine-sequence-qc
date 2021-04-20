d20fff85b9cbc90e62b91c7282b6a402fc48361a
Dan Fornika <dfornika@gmail.com>
2021-04-19 17:39:18 -0700

    Cannot use --name and --prefix together

--- .github/scripts/install_ncbi-acc-download.sh
+++ .github/scripts/install_ncbi-acc-download.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 
 echo "Install NCBI Genome Download tool using conda.." >> artifacts/test_artifact.log
 
-conda create --yes --name ncbi-acc-download --prefix /opt/miniconda3/envs/ncbi-acc-download
+conda create --yes --prefix /opt/miniconda3/envs/ncbi-acc-download

bb3fdabb42414bd9c092930fce3f2e81a4dbcff2
Dan Fornika <dfornika@gmail.com>
2021-04-21 12:35:14 -0700

    do not copy Data directory from example InterOp data

--- .github/scripts/download_example_interop_data.sh
+++ .github/scripts/download_example_interop_data.sh
@@ -9,7 +9,9 @@ wget http://cf.10xgenomics.com/supp/cell-exp/cellranger-tiny-bcl-1.2.0.tar.gz
 
 tar -xzf cellranger-tiny-bcl-1.2.0.tar.gz && rm cellranger-tiny-bcl-1.2.0.tar.gz
 
-mv cellranger-tiny-bcl-1.2.0/* mock_runs/210101_M00000_0000_000000000-A1B2C
+mv cellranger-tiny-bcl-1.2.0/InterOp mock_runs/210101_M00000_0000_000000000-A1B2C
+mv cellranger-tiny-bcl-1.2.0/RunInfo.xml mock_runs/210101_M00000_0000_000000000-A1B2C
+mv cellranger-tiny-bcl-1.2.0/runParameters.xml mock_runs/210101_M00000_0000_000000000-A1B2C
 
 rm -r cellranger-tiny-bcl-1.2.0
 

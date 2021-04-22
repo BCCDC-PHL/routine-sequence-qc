3f90b9e75dfc5cbe2c1c3964c65644b232c733fa
Dan Fornika <dfornika@gmail.com>
2021-04-22 12:55:48 -0700

    Put config flag in correct location

--- .github/scripts/test_against_previous_release.sh
+++ .github/scripts/test_against_previous_release.sh
@@ -7,7 +7,7 @@ export PATH=/opt/nextflow/bin:$PATH
 
 # write test log as github Action artifact
 echo "Nextflow run current PR..." >> artifacts/test_artifact.log
-NXF_VER=20.10.0 nextflow -quiet run ./main.nf \
+NXF_VER=20.10.0 nextflow -C ${PWD}/../.github/config/nextflow.config -quiet run ./main.nf \
        -C ${PWD}/.github/config/nextflow.config \ 
        -profile conda \
        --cache ${HOME}/.conda/envs \
@@ -25,8 +25,7 @@ pushd previous_release
 git checkout adfffed374ae0212b707b042233652704286b4d7 -b previous-release
 
 echo "Nextflow run previous release..." >> ../artifacts/test_artifact.log
-NXF_VER=20.10.0 nextflow -quiet run ./main.nf \
-       -C ${PWD}/../.github/config/nextflow.config \ 
+NXF_VER=20.10.0 nextflow -C ${PWD}/../.github/config/nextflow.config -quiet run ./main.nf \
        -profile conda \
        --cache ${HOME}/.conda/envs \
        --kraken2_db ${PWD}/../.github/data/kraken2_db \

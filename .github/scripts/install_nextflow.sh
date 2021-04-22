9926b4b38c44877e23b918780eb7bb874426a911
Dan Fornika <dfornika@gmail.com>
2021-04-22 12:44:00 -0700

    Use github config during initial nextflow run

--- .github/scripts/install_nextflow.sh
+++ .github/scripts/install_nextflow.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 echo Install Nextflow .. >> artifacts/test_artifact.log
 
-wget -qO- https://get.nextflow.io | bash
+wget -qO- https://get.nextflow.io | bash > nextflow_install.out 2> nextflow_install.err
 
 mkdir -p /opt/nextflow/bin
 
@@ -13,4 +13,6 @@ echo "export PATH=/opt/nextflow/bin:$PATH" >> ~/.bashrc
 
 export PATH=/opt/nextflow/bin:$PATH
 
-NXF_VER=20.10.0 nextflow run hello
+NXF_VER=20.10.0 nextflow run -C .github/data/config/nextflow.config hello
+
+mv nextflow_install.out nextflow_install.err artifacts

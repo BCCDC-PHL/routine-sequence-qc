3542e632574db5041c73aedd7cfa3ffcc067ff23
Dan Fornika <dfornika@gmail.com>
2021-04-22 12:36:24 -0700

    Invoke nextflow to ensure all deps installed before caching

--- .github/scripts/install_nextflow.sh
+++ .github/scripts/install_nextflow.sh
@@ -10,3 +10,7 @@ mkdir -p /opt/nextflow/bin
 mv nextflow /opt/nextflow/bin
 
 echo "export PATH=/opt/nextflow/bin:$PATH" >> ~/.bashrc
+
+export PATH=/opt/nextflow/bin:$PATH
+
+NXF_VER=20.10.0 nextflow run hello

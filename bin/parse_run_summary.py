a90ce77c34f8d5fd558100db2028659e70571ee8
Dan Fornika <dfornika@gmail.com>
2021-08-11 23:31:22 -0700

    Re-factor for consistency with BaseSpace API

--- bin/parse_run_summary.py
+++ bin/parse_run_summary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import argparse
+import collections
 import sys
 import re
 import json
@@ -9,24 +10,40 @@ def parse_read_summary(summary_path):
     read_summary_headers = []
     read_summary_lines = []
 
-    replaced_fields = {'%>=q30': 'percent_greater_than_q30',
-                       '%_occupied': 'percent_occupied'}
+    replaced_fields = {
+        '%>=Q30': 'PercentGtQ30',
+        'Projected Yield': 'ProjectedTotalYield',
+        'Yield': 'YieldTotal',
+        'Error Rate': 'ErrorRate',
+        'Aligned': 'PercentAligned',
+        '% Occupied': 'PercentOccupied',
+        'Level': 'ReadNumber',
+        'Intensity C1': 'IntensityCycle1',
+    }
+
+    level_to_read_number = {
+        'Read 1': 1,
+        'Read 2 (I)': 2,
+        'Read 3 (I)': 3,
+        'Read 4': 4,
+    }
 
     with open(summary_path) as summary:
         for line in summary:
             if re.match("^Level", line):
                 read_summary_headers = re.split("\s*,", line.rstrip())
-                read_summary_headers = [
-                    x.lower().replace(" ", "_") for x in read_summary_headers
-                ]
+                #read_summary_headers = [
+                #    x.replace(" ", "") for x in read_summary_headers
+                #]
+                print(read_summary_headers)
                 for idx, header in enumerate(read_summary_headers):
                     if header in replaced_fields:
                         read_summary_headers[idx] = replaced_fields[header]
-                
+                print(read_summary_headers)
                 break
         for line in summary:
-            if re.match("^Total", line):
-                read_summary_lines.append(re.split(",", line.rstrip()))
+            if re.match("^Total", line) or re.match("^Non-indexed", line):
+                # read_summary_lines.append(re.split(",", line.rstrip()))
                 break
             else:
                 read_summary_lines.append(re.split(",", line.rstrip()))
@@ -35,10 +52,19 @@ def parse_read_summary(summary_path):
     for line in read_summary_lines:
         read_summary_line_dict = {}
         for idx, header in enumerate(read_summary_headers):
-            if header == 'level':
-                read_summary_line_dict[header] = line[idx]
-            elif header == 'intensity_c1':
+            if header == 'ReadNumber':
+                read_summary_line_dict[header] = level_to_read_number[line[idx]]
+                if re.search("(I)", line[idx]):
+                    read_summary_line_dict['IsIndexed'] = True
+                else:
+                    read_summary_line_dict['IsIndexed'] = False
+            elif header == 'IntensityC1':
                 read_summary_line_dict[header] = int(line[idx])
+            elif header == 'ErrorRate':
+                if line[idx] == 'nan':
+                    read_summary_line_dict[header] = 0
+                else:
+                    read_summary_line_dict[header] = float(line[idx])
             else:
                 read_summary_line_dict[header] = float(line[idx])
         read_summary.append(read_summary_line_dict)
@@ -46,106 +72,174 @@ def parse_read_summary(summary_path):
     return read_summary
 
 
-def parse_read_summary_detail(summary_path):
-    headers = [
-        'lane',
-        'surface',
-        'tiles',
-        'density',
-        'clusters_passing_filter',
-        'legacy_phasing_prephasing_rate',
-        'phasing_slope_offset',
-        'prephasing_slope_offset',
-        'reads',
-        'reads_passing_filter',
-        'percent_greater_than_q30',
-        'yield',
-        'cycles_error',
-        'aligned',
-        'error',
-        'error_35',
-        'error_75',
-        'error_100',
-        'percent_occupied',
-        'intensity_at_cycle_1',
+def parse_lanes_by_read(summary_path):
+    headers_input_order = [
+        'LaneNumber',
+        'Surface',
+        'TileCount',
+        'Density',
+        'PercentPf',
+        'LegacyPhasingPrephasingRate',
+        'Phasing',
+        'Prephasing',
+        'Reads',
+        'ReadsPf',
+        'PercentGtQ30',
+        'Yield',
+        'CyclesError',
+        'PercentAligned',
+        'ErrorRate',
+        'ErrorRate35',
+        'ErrorRate75',
+        'ErrorRate100',
+        'Occupancy',
+        'IntensityCycle1',
     ]
+
+    headers_output_order = [
+        'ReadNumber',
+        'LaneNumber',
+        'TileCount',
+        'Density',
+        'DensityDeviation',
+        'PercentPf',
+        'PercentPfDeviation',
+        'Reads',
+        'ReadsPf',
+        'PercentGtQ30',
+        'Yield',
+        'CyclesError',
+        'PercentAligned',
+        'PercentAlignedDeviation',
+        'ErrorRate',
+        'ErrorRateDeviation',
+        'ErrorRate35',
+        'ErrorRate35Deviation',
+        'ErrorRate75',
+        'ErrorRate75Deviation',
+        'ErrorRate100',
+        'ErrorRate100Deviation',
+        'IntensityCycle1',
+        'IntensityCycle1Deviation',
+        'PhasingSlope',
+        'PhasingOffset',
+        'PrePhasingSlope',
+        'PrePhasingOffset',
+        'ClusterDensity',
+        'Occupancy',
+    ]
+
     average_stdev_fields = [
-        'aligned',
-        'clusters_passing_filter',
-        'density',
-        'error',
-        'error_100',
-        'error_75',
-        'error_35',
-        'intensity_at_cycle_1',
-        'percent_occupied',
+        'PercentAligned',
+        'PercentPf',
+        'Density',
+        'ErrorRate',
+        'ErrorRate35',
+        'ErrorRate75',
+        'ErrorRate100',
+        'IntensityCycle1',
+        'Occupancy',
     ]
-    slash_fields = { 'legacy_phasing_prephasing_rate': {'numerator_field': 'legacy_phasing_rate',
-                                                        'denominator_field': 'legacy_prephasing_rate'},
-                     'phasing_slope_offset': {'numerator_field': 'phasing_slope',
-                                              'denominator_field': 'phasing_offset'},
-                     'prephasing_slope_offset': {'numerator_field': 'prephasing_slope',
-                                                 'denominator_field': 'prephasing_offset'},
+
+    slash_fields = { 
+        'Phasing': {
+            'numerator_field': 'PhasingSlope',
+            'denominator_field': 'PhasingOffset'
+        },
+        'Prephasing': {
+            'numerator_field': 'PrePhasingSlope',
+            'denominator_field': 'PrePhasingOffset'
+        },
     }
+
     float_fields = [
-        'percent_greater_than_q30',
-        'reads',
-        'reads_passing_filter',
-        'yield',
+        'PercentGtQ30',
+        'Reads',
+        'ReadsPf',
+        'Yield',
+    ]
+
+    int_fields = [
+        'ReadNumber',
+        'LaneNumber',
+        'TileCount',
     ]
     
-    lines_by_read = {
-        'read_1': [],
-        'read_i1': [],
-        'read_i2': [],
-        'read_2': [],
-    }
+    lanes_by_read = []
+
     with open(summary_path) as summary:
-        current_read = None
+        read_number = None
         for line in summary:
             if re.match("^Read 1\n$", line):
-                current_read = 'read_1'
+                read_number = 1
             elif re.match("^Read 2 \(I\)\n$", line):
-                current_read = 'read_i1'
+                read_number = 2
             elif re.match("^Read 3 \(I\)\n$", line):
-                current_read = 'read_i2'
+                read_number = 3
             elif re.match("^Read 4$\n", line):
-                current_read = 'read_2'
+                read_number = 4
             elif re.match("^Extracted", line) or re.match("^Called", line) or re.match("^Scored", line):
-                current_read = None
-            elif current_read and not re.match("^Lane", line):
-                read_line = [record.strip() for record in line.strip().split(',')]
-                read_line_dict = dict(zip(headers, read_line))
-                lines_by_read[current_read].append(read_line_dict)
+                read_number = None
             else:
                 pass
+                
+            if read_number and not re.match("^Lane", line) and not re.match("^Read", line):
+                read_line = [record.strip() for record in line.strip().split(',')]
+                read_line_dict = dict(zip(headers_input_order, read_line))
+                read_line_dict['ReadNumber'] = read_number
+            else:
+                read_line_dict = {}
 
-        for field in average_stdev_fields:
-            string_value = read_line_dict[field]
-            [average, stdev] = [float(value) for value in string_value.split(' +/- ')]
-            read_line_dict[field] = { 'average': average,
-                                      'stdev': stdev }
+            if read_line_dict:
+                for field in average_stdev_fields:
+                    string_value = read_line_dict[field]
+                    [average, stdev] = [float(value) for value in string_value.split(' +/- ')]
+                    deviation_field = field + 'Deviation'
+                    read_line_dict[field] = average
+                    read_line_dict[deviation_field] = stdev
 
-        for field, num_denom in slash_fields.items():
-            string_value = read_line_dict[field]
-            numerator_field = num_denom['numerator_field']
-            denominator_field = num_denom['denominator_field']
-            [numerator, denominator] = [float(value) for value in string_value.split(' / ')]
-            read_line_dict[numerator_field] = numerator
-            read_line_dict[denominator_field] = denominator
-            read_line_dict.pop(field, None)
+                for field, num_denom in slash_fields.items():
+                    string_value = read_line_dict[field]
+                    numerator_field = num_denom['numerator_field']
+                    denominator_field = num_denom['denominator_field']
+                    [numerator, denominator] = [float(value) for value in string_value.split(' / ')]
+                    read_line_dict[numerator_field] = numerator
+                    read_line_dict[denominator_field] = denominator
+                    read_line_dict.pop(field, None)
+
+                for field in float_fields:
+                    read_line_dict[field] = float(read_line_dict[field])
+
+                for field in int_fields:
+                    read_line_dict[field] = int(read_line_dict[field])
+
+                if read_line_dict['Surface'] == '-':
+                    read_line_dict['Density'] = int(float(read_line_dict['Density'] * 1000))
+                    read_line_dict['ClusterDensity'] = read_line_dict['Density']
+                    read_line_dict['Reads'] = int(float(read_line_dict['Reads'] * 1000000))
+                    read_line_dict['ReadsPf'] = int(float(read_line_dict['ReadsPf'] * 1000000))
+
+                    read_line_dict.pop('OccupancyDeviation', None)
+                    read_line_dict.pop('LegacyPhasingPrephasingRate', None)
+                    read_line_dict.pop('Surface', None)
+
+                    read_line_dict_ordered = collections.OrderedDict(sorted(read_line_dict.items(), key=lambda x: headers_output_order.index(x[0])))
+                
+                    lanes_by_read.append(read_line_dict_ordered)
     
-    return lines_by_read
+    return lanes_by_read
 
 
 def main(args):
-    read_summary = parse_read_summary(args.summary)
-    read_summary_detail = parse_read_summary_detail(args.summary)
+    reads = parse_read_summary(args.summary)
+    lanes_by_read = parse_lanes_by_read(args.summary)
 
-    output = {'read_summary': read_summary,
-              'read_details': read_summary_detail}
-    # print(json.dumps(read_summary))
-    print(json.dumps(output))
+    output = {
+        'Reads': reads,
+        'LanesByRead': lanes_by_read,
+    }
+
+    print(json.dumps(output, indent=2))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()

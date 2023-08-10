2644de2a1ac188a9ce9c8a2657056e59172c65c5
Dan Fornika <dfornika@gmail.com>
2023-08-09 17:35:44 -0700

    Add unclassified reads to top n abundances (#73)

--- bin/bracken_top_n_linelist.py
+++ bin/bracken_top_n_linelist.py
@@ -24,14 +24,21 @@ def parse_bracken_report(bracken_report_path):
 def main(args):
     bracken_report = parse_bracken_report(args.bracken_report)
 
-    bracken_report_sorted = sorted(bracken_report, key=lambda k: k['fraction_total_reads'], reverse=True) 
+    fraction_unclassified_reads = 0.0
+    unclassified_reads_records = list(filter(lambda x: x['taxonomy_id'] == '0', bracken_report))
+    if len(unclassified_reads_records) > 0:
+        unclassified_reads_record = unclassified_reads_records[0]
+        fraction_unclassified_reads = unclassified_reads_record['fraction_total_reads']
+
+    bracken_report_without_unclassified = list(filter(lambda x: x['taxonomy_id'] != '0', bracken_report))
+    bracken_report_sorted = sorted(bracken_report_without_unclassified, key=lambda k: k['fraction_total_reads'], reverse=True) 
     
     output_fields = ['sample_id', 'taxonomy_level']
     output_line = {
         'sample_id': args.sample_id,
         'taxonomy_level': args.taxonomy_level,
     }
-    
+
     for n in range(args.top_n):
         num = str(n + 1)
         name_field = 'abundance_' + num + '_name'
@@ -54,7 +61,16 @@ def main(args):
         except IndexError as e:
             output_line[fraction_total_reads_field] = 0.0
         output_fields.append(fraction_total_reads_field)
-        
+
+    unclassified_fields = [
+        'abundance_unclassified_name',
+        'abundance_unclassified_taxonomy_id',
+        'abundance_unclassified_fraction_total_reads',
+    ]
+    output_fields += unclassified_fields
+    output_line['abundance_unclassified_name'] = 'unclassified'
+    output_line['abundance_unclassified_taxonomy_id'] = '0'
+    output_line['abundance_unclassified_fraction_total_reads'] = fraction_unclassified_reads
 
     writer = csv.DictWriter(sys.stdout, fieldnames=output_fields, dialect='unix', quoting=csv.QUOTE_MINIMAL)
     writer.writeheader()
